e0fe1a10e7edf8c3f378511e403413e47597c859
jiyePark <parkji3618@naver.com>
2022-10-20 17:51:38 +0900

    feat: add flask서버 음성파일 받기 기능 구현

--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename
 
 app=Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return 'Hello World!'
+@app.route("/recordings", methods=['GET', 'POST'])
+def recordings():
+    if request.method=='POST':
+        print("hello")
+        file = request.files['file']
+        file.save(secure_filename(file.filename))
+        return 'uploads 음성 파일 성공!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
