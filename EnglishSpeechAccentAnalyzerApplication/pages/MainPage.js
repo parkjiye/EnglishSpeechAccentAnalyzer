9dc69fdd2a5d56a6629e4f3d9b4effe870c9bcc1
jiyePark <parkji3618@naver.com>
2022-10-27 13:12:15 +0900

    feat: MainPage 업데이트

--- EnglishSpeechAccentAnalyzerApplication/pages/MainPage.js
+++ EnglishSpeechAccentAnalyzerApplication/pages/MainPage.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 import { Audio } from 'expo-av';
 
+
 const StatusBarHeight = Platform.OS === 'ios' ? getStatusBarHeight(true) : StatusBar.currentHeight;
 
 export default function MainPage({ navigation, route }) {
@@ -12,24 +13,28 @@ export default function MainPage({ navigation, route }) {
     const [sound, setSound] = React.useState();
 
     const analyze = async (e) => {
-        const audio  = recording.getURI();
+        const audio = recording.getURI();
         const formData = new FormData();
-        formData.append('file', {uri: audio, name:'test.m4a', type:'audio/m4a'});
+        formData.append('file', { uri: audio, name: 'test.m4a', type: 'audio/m4a' });
         formData.append("native_language", 11);
         formData.append("sex", 0);
 
         const api = async () => {
             console.log("api start!");
-            const res = await axios.post("http://127.0.0.1:5000/recordings", formData, {headers:{'Content-Type': 'multipart/form-data', }});
+            const res = await axios.post("http://127.0.0.1:5000/recordings", formData, { headers: { 'Content-Type': 'multipart/form-data', } });
+            navigation.navigate("Result", {result:res.data});
             return res.data;
         }
 
         api().then(res => {
             console.log("success: ");
-            console.log(res);})
+            console.log(res);
+        })
             .catch(err => {
                 console.log("fail: ");
-                console.log(err);});
+                console.log(err);
+            });
+
     }
 
     async function startRecording() {
@@ -89,6 +94,8 @@ export default function MainPage({ navigation, route }) {
             <View style={styles.Audio}>
                 <Button title={isRecording ? '녹음 중단' : '녹음 시작'} onPress={isRecording ? stopRecording : startRecording} />
                 <Button title="녹음 재생" onPress={playSound} />
+            </View>
+            <View style={styles.footer}>
                 <Button title="분석하기" onPress={analyze} />
             </View>
         </View>
@@ -108,21 +115,23 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     headerText: {
-
+        fontSize:18
     },
     TextBox: {
         height: '18%',
         justifyContent: "center",
         alignItems: "center",
-        marginHorizontal: 20
+        marginHorizontal: 20,
+        marginBottom:20,
+        flex:1
     },
     TextLine: {
-
+        fontSize:15,
     },
     Audio: {
         flex: 1,
+    },    
+    footer:{
+        flex:1,
     },
-    footer: {
-
-    }
 });
